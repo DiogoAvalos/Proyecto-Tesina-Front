605d192c1c4c1c16dd161a3687bf7bab28e2716a
Oddi <133927308+DiogoAvalos@users.noreply.github.com>
2024-07-24 19:07:28 -0500

    actualizando directorios

---
+++ src/app/components/tabladinamica/tabladinamica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabladinamica',
+  templateUrl: './tabladinamica.component.html',
+  styleUrls: ['./tabladinamica.component.css']
+})
+export class TabladinamicaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
