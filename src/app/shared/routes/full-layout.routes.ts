eaa6c42e1ff0f0071bb6c7d29f26e5ed3742b69a
Oddi <133927308+DiogoAvalos@users.noreply.github.com>
2024-08-29 23:21:19 -0500

    agregando validación de token

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -1,40 +1,41 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/auth/services/auth.guard';
 
 //Route for content layout with sidebar, navbar and footer.
 
 export const Full_ROUTES: Routes = [
     {
-        path: 'dashboard',
-        loadChildren: () => import('../../modules/dashboard/dashboard.module').then(m => m.DashboardModule)
-    },
-
-    {
-        path: 'components',
-        loadChildren: () => import('../../components/components.module').then(m => m.ComponentsModule)
-    },
-
-    {
-        path: 'forms',
-        loadChildren: () => import('../../forms/forms.module').then(m => m.FormsModule)
-    },
-
-    {
-        path: 'widgets',
-        loadChildren: () => import('../../widgets/widgets.module').then(m => m.WidgetsModule)
-    },
-
-    {
-        path: 'charts',
-        loadChildren: () => import('../../charts/charts.module').then(m => m.ChartsModule)
-    },
-
-    {
-        path: 'profile',
-        loadChildren: () => import('../../modules/profile/profile.module').then(m => m.ProfileModule)
-    },
-
-    {
-        path: 'modules',
-        loadChildren: () => import('../../modules/auth-routing.module').then(m => m.AuthRoutingModule)
+        path: '',
+        canActivate: [AuthGuard],
+        children: [
+            {
+                path: 'dashboard',
+                loadChildren: () => import('../../modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+            },
+            {
+                path: 'components',
+                loadChildren: () => import('../../components/components.module').then(m => m.ComponentsModule)
+            },
+            {
+                path: 'forms',
+                loadChildren: () => import('../../forms/forms.module').then(m => m.FormsModule)
+            },
+            {
+                path: 'widgets',
+                loadChildren: () => import('../../widgets/widgets.module').then(m => m.WidgetsModule)
+            },
+            {
+                path: 'charts',
+                loadChildren: () => import('../../charts/charts.module').then(m => m.ChartsModule)
+            },
+            {
+                path: 'profile',
+                loadChildren: () => import('../../modules/profile/profile.module').then(m => m.ProfileModule)
+            },
+            {
+                path: 'modules',
+                loadChildren: () => import('../../modules/auth-routing.module').then(m => m.AuthRoutingModule)
+            }
+        ]
     }
-];
+]
